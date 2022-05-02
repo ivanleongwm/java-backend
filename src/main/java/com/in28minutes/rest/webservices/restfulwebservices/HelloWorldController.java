534667b0bc82375ddfa3720d22839452bce92cd4
ivanleongwm <ivanleongwm@gmail.com>
2022-05-02 16:55:09 +0800

    helloworld java bean added

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.*;
 public class HelloWorldController {
     // method -- "hello world"
     // uri ---
-    @RequestMapping(method=RequestMethod.GET,path="/hello-world")
+    //@RequestMapping(method=RequestMethod.GET,path="/hello-world")
+    @GetMapping(path="/hello-world")
     public String HelloWorld() {
         return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean HelloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
